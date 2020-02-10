fde6f35f0f475fb1b95457d2ae6269c3004b4cff
Zach <zmccleaf@andculture.com>
2020-02-10 16:08:26 -0500

    adding form

--- src/components/organisms/ContactForm.js
+++ src/components/organisms/ContactForm.js
@@ -4,6 +4,7 @@ class ContactForm extends React.Component {
 
     render() {
         return (
+            <div>
             <form className = "o-contact-form">
                 <h6>Get In Touch</h6>
                 <header>what can we help you with today?</header>
@@ -24,6 +25,27 @@ class ContactForm extends React.Component {
                     <label htmlFor="start-ups">talk start-ups (catamaran)</label>
                 </fieldset>
             </form>
+
+<form name="contact" method="POST" data-netlify="true">
+<p>
+  <label>Your Name: <input type="text" name="name" /></label>   
+</p>
+<p>
+  <label>Your Email: <input type="email" name="email" /></label>
+</p>
+<p>
+  <label>Your Role: <select name="role[]" multiple>
+    <option value="leader">Leader</option>
+    <option value="follower">Follower</option>
+  </select></label>
+</p>
+<p>
+  <label>Message: <textarea name="message"></textarea></label>
+</p>
+<p>
+  <button type="submit">Send</button>
+</p>
+</form></div>
         )
     }
 }
