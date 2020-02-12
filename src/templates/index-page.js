a842e3faf7a136632e9a22c685cb07a2819d7931
Zach McCleaf <zmccleaf@andculture.com>
2020-02-12 11:44:08 -0500

    adding text styles (#1)
    * adding text styles
    
    * fixing font-size and line-height values
    
    * fixing easter egg font style

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -14,6 +14,11 @@ export const IndexPageTemplate = ({
   <div>
     <div>
       <h1>{ title }</h1>
+      <h2>{ title }</h2>
+      <h3>{ title }</h3>
+      <h4>{ title }</h4>
+      <h5>{ title }</h5>
+      <h6>{ title }</h6>
     </div>
     <div>
       { secondaryTitle }
