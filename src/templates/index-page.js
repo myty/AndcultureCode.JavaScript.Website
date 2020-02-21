9265feefe93f8348e3dc75e70e8fcd6a4cb97595
Zach <zmccleaf@andculture.com>
2020-02-21 13:49:48 -0500

    fixing index

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -42,7 +42,7 @@ export const IndexPageTemplate = ({
 )
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
   return (
     <Layout>
       <IndexPageTemplate
