f841cda874aa94ca32f6814adbf71b40db4a87f9
Zach <zmccleaf@andculture.com>
2020-02-25 10:38:11 -0500

    edits per Brian and Greg's notes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -7,14 +7,9 @@ import ContactForm from '../components/organisms/ContactForm'
 import HomePageAnimation from '../components/organisms/HomeAnimation'
 import EasterEgg from '../components/atoms/EasterEgg'
 
-import jigglyDoo from './../../static/img/jiggly-01.svg';
-import greenBlob from './../../static/img/greenblob.png';
-import dots from './../../static/img/center_lane_dots.png';
-import terrazo from './../../static/img/terrazo.png';
-import greenBrush from './../../static/img/greenbrush.png';
-
 import { gsap } from 'gsap';
 import { TimelineMax, Power1 } from 'gsap';
+import Hero from '../components/molecules/Hero'
 gsap.registerPlugin(TimelineMax)
 gsap.registerPlugin(Power1)
 
@@ -25,25 +20,7 @@ export const IndexPageTemplate = ({
 }) => (
     <div>
       <HomePageAnimation />
-      <div className = "o-hero">
-        <img className = "o-hero__background -jigglydoo" src = { jigglyDoo } />
-        <img className = "o-hero__background -dots" src = { dots } />
-        <img className = "o-hero__background -greenblob" src = { greenBlob } />
-        <img className = "o-hero__background -terrazo" src = { terrazo } />
-        <img className = "o-hero__background -greenbrush" src = { greenBrush } />
-        <div className = "o-rhythm__container">
-          <div className = "o-hero__top">
-            <div className = "o-hero__titles">
-                <h1 className = "-after">{ title }</h1>
-                <p className = "-before">{ secondaryTitle }</p>
-            </div>
-          </div>
-          <div className = "o-hero__subtitles">
-            <h3>{ subTitle }</h3>
-          </div>
-          <h6>A Design Company</h6>
-        </div>
-      </div>
+      <Hero title = { title } secondaryTitle = { secondaryTitle } subTitle = { subTitle } />
     </div>
 )
 
