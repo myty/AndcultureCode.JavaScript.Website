4a28b72fe85e9ba3750e7e8bcfb658f1fcd24d3a
Zach <zmccleaf@andculture.com>
2020-02-26 09:54:50 -0500

    adding wordmark in header

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'; 
-import logo from '../../../static/img/ac_Logo.svg'
+import logo from '../../../static/img/ac_Logo.svg';
+import wordmark from '../../../static/img/ac-wordmark.svg'
 
 const Header = class extends React.Component {
   constructor(props) {
@@ -8,21 +9,26 @@ const Header = class extends React.Component {
 
   render() {
     return (
-      <header className = "m-header">
-        <div className = "o-rhythm__container -fluid">
-          <a 
-            href      = "/" 
-            className =  "m-header__logo">
-            <img src = { logo } alt="andculture" />
-          </a>
-          <nav 
-            className  = "m-header__navigation" 
-            role       = "navigation" 
-            aria-label = "main-navigation">
-            <a href="/contact"><span></span>contact</a>
-          </nav>
+      <div>
+        <header className = "m-header">
+          <div className = "o-rhythm__container -fluid">
+            <a 
+              href      = "/" 
+              className =  "m-header__logo">
+              <img src = { logo } alt="andculture" />
+            </a>
+            <nav 
+              className  = "m-header__navigation" 
+              role       = "navigation" 
+              aria-label = "main-navigation">
+              <a href="/contact"><span></span>contact</a>
+            </nav>
+          </div>
+        </header>
+        <div className = "m-header__title">
+          <img src = { wordmark } alt="andculture" />
         </div>
-      </header>
+      </div>
      
     )
   }
