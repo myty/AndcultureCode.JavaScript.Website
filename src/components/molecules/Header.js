704cbc79370f684d8a89c33228a14be962371bda
Zach <zmccleaf@andculture.com>
2020-03-09 14:27:04 -0400

    development

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import logo from '../../../static/img/ac_Logo.svg';
 import wordmark from '../../../static/img/ac-wordmark.svg';
 
@@ -10,32 +10,32 @@ const Header = class extends React.Component {
   render() {
     return (
       <div>
-        <header className = "m-header">
-          <div className = "o-rhythm__container -fluid">
+        <header className="m-header">
+          <div className="o-rhythm__container -fluid">
             <a
-              href      = "/"
-              className =  "m-header__logo">
-              <img src = { logo } alt="andculture" />
+              href="/"
+              className="m-header__logo">
+              <img src={logo} alt="andculture" />
             </a>
             { // if
               !this.props.hideNavigation &&
               <nav
-                className  = "m-header__navigation"
-                role       = "navigation"
-                aria-label = "main-navigation">
+                className="m-header__navigation"
+                role="navigation"
+                aria-label="main-navigation">
                 <a href="/contact"><span></span>contact</a>
               </nav>
             }
           </div>
         </header>
-        <div className = "m-header__title">
+        <div className="m-header__title">
           { // if
             this.props.pageTitle != "home" &&
-              <div className = "m-header__page-title">{ this.props.pageTitle }</div>
+            <div className="m-header__page-title">{this.props.pageTitle}</div>
           }
           { // if
             this.props.pageTitle === "home" &&
-              <img src = { wordmark } alt="andculture" />
+            <img src={wordmark} alt="andculture" />
           }
         </div>
       </div>
