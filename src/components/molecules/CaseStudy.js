3dcf987de462553adb0a9e399f59139f8040f64a
Zach <zmccleaf@andculture.com>
2020-03-12 08:51:36 -0400

    adding mobile visibility styles to case study list

--- src/components/molecules/CaseStudy.js
+++ src/components/molecules/CaseStudy.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CoverTransitionLink from '../organisms/CoverTransitionLink';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const CaseStudy = class extends React.Component {
 
@@ -7,11 +8,29 @@ const CaseStudy = class extends React.Component {
         super(props);
 
         this.state = {
-            isHovered: false
+            isHovered: false,
+            isVisible: false,
+            isMobile: window.innerWidth < 1024 ? true : false
         };
 
         this._handleHover            = this._handleHover.bind(this);
         this._handleUnhover          = this._handleUnhover.bind(this);
+        this._onChange               = this._onChange.bind(this);
+        this._isMobile               = this._isMobile.bind(this);
+    }
+
+    _onChange(isVisible) {
+        this.setState({
+            isVisible: isVisible ? true : false,
+        });
+    }
+
+    _isMobile() {
+        if (window.innerWidth < 1024) {
+            this.setState({
+                isMobile: true,
+            });
+        }
     }
 
     _handleHover() {
@@ -24,7 +43,7 @@ const CaseStudy = class extends React.Component {
         this.setState({
             isHovered: false
         });
-        
+
         setTimeout(this._handleUnhoverAnimation, 600);
     }
 
@@ -32,7 +51,10 @@ const CaseStudy = class extends React.Component {
         let caseStudyClass = "m-case-study";
         if (this.state.isHovered) {
             caseStudyClass += " -hovered"
-        } 
+        }
+        if (this.state.isVisible) {
+            caseStudyClass += " -visible";
+        }
         if (this.props.post.frontmatter.secondaryLayout) {
             caseStudyClass += " -secondary";
         }
@@ -43,11 +65,11 @@ const CaseStudy = class extends React.Component {
             borderColor: this.props.post.frontmatter.color,
         }
 
-        if (this.state.isHovered) {
+        if (this.state.isHovered || this.state.isVisible && this.state.isMobile) {
             caseStudyStyle = {
                 backgroundColor: this.props.post.frontmatter.color,
             }
-        } 
+        }
 
         if (this.state.isHovered) {
             linkStyle = {
@@ -57,40 +79,42 @@ const CaseStudy = class extends React.Component {
         }
 
         return (
-            <article className = { caseStudyClass } style = { caseStudyStyle } key = { this.props.post.id } onMouseLeave = { this._handleUnhover }>
-                <div className = "m-case-study__block -top"></div>
-                <div className = "o-rhythm__container">
-                    <div className = "m-case-study__image-container">
-                        { this.props.post.frontmatter.featuredimage ? (
-                        <div className = "m-case-study__image">
-                            <img src = { this.props.post.frontmatter.featuredimage.childImageSharp.fluid.src } />
-                        </div>
-                        ) : null }
-                        <div className = "m-case-study__texture">
-                            <img src = { this.props.post.frontmatter.texture } />
-                        </div>
-                    </div>
-                    <div className = "m-case-study__content">
-                        <div className = "m-case-study__content-inner">
-                            <div className = "m-case-study__count">
-                                { this.props.count }<span>/{ this.props.total }</span>
+            <VisibilitySensor onChange = { this._onChange }>
+                <article className = { caseStudyClass } style = { caseStudyStyle } key = { this.props.post.id } onMouseLeave = { this._handleUnhover }>
+                    <div className = "m-case-study__block -top"></div>
+                    <div className = "o-rhythm__container">
+                        <div className = "m-case-study__image-container">
+                            { this.props.post.frontmatter.featuredimage ? (
+                            <div className = "m-case-study__image">
+                                <img src = { this.props.post.frontmatter.featuredimage.childImageSharp.fluid.src } />
                             </div>
-                            <h5>{ this.props.post.frontmatter.partnerName }</h5>
-                            <p>
-                                { this.props.post.frontmatter.title }
-                            </p>
-                            <CoverTransitionLink
-                                onMouseEnter = { this._handleHover }
-                                className = "a-button"
-                                to        = { this.props.post.fields.slug }
-                                style     = { linkStyle }>
-                                See How
-                            </CoverTransitionLink>
+                            ) : null }
+                            <div className = "m-case-study__texture">
+                                <img src = { this.props.post.frontmatter.texture } />
+                            </div>
+                        </div>
+                        <div className = "m-case-study__content">
+                            <div className = "m-case-study__content-inner">
+                                <div className = "m-case-study__count">
+                                    { this.props.count }<span>/{ this.props.total }</span>
+                                </div>
+                                <h5>{ this.props.post.frontmatter.partnerName }</h5>
+                                <p>
+                                    { this.props.post.frontmatter.title }
+                                </p>
+                                <CoverTransitionLink
+                                    onMouseEnter = { this._handleHover }
+                                    className = "a-button"
+                                    to        = { this.props.post.fields.slug }
+                                    style     = { linkStyle }>
+                                    See How
+                                </CoverTransitionLink>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className = "m-case-study__block -bottom"></div>
-            </article>
+                    <div className = "m-case-study__block -bottom"></div>
+                </article>
+            </VisibilitySensor>
         )
     }
 }
