79253b64f034a49d22388a580f9cd3660920126d
Zach <zmccleaf@andculture.com>
2020-03-19 13:40:56 -0400

    changing homepage hero animation

--- src/components/organisms/HeroAnimation.js
+++ src/components/organisms/HeroAnimation.js
@@ -13,25 +13,25 @@ import orangSicle from './../../../static/img/homepage-hero/orangsicle.png';
 
 const HeroAnimation = () => {
     useEffect(() => {
-        const groupOne = document.querySelector('.group-one');
-        const groupOneImgOne = groupOne.querySelector('.-pink-blob');
-        const groupOneImgTwo = groupOne.querySelector('.-blue-blob-two');
-        const groupOneImgThree = groupOne.querySelector('.-doodles');
-        const groupTwo = document.querySelector('.group-two');
-        const groupTwoImgOne = groupTwo.querySelector('.-green-paint');
-        const groupTwoImgTwo = groupTwo.querySelector('.-matrix');
         const groupThree = document.querySelector('.group-three');
-        const groupThreeImgOne = groupThree.querySelector('.-orangsicle');
-        const groupThreeImgTwo = groupThree.querySelector('.-pink-elephant');
-        const groupThreeImgThree = groupThree.querySelector('.-blue-blob');
+        const groupThreeImgOne = groupThree.querySelector('.-pink-blob');
+        const groupThreeImgTwo = groupThree.querySelector('.-blue-blob-two');
+        const groupThreeImgThree = groupThree.querySelector('.-doodles');
+        // const groupTwo = document.querySelector('.group-two');
+        // const groupTwoImgOne = groupTwo.querySelector('.-green-paint');
+        // const groupTwoImgTwo = groupTwo.querySelector('.-matrix');
+        const groupOne = document.querySelector('.group-one');
+        const groupOneImgOne = groupOne.querySelector('.-orangsicle');
+        const groupOneImgTwo = groupOne.querySelector('.-pink-elephant');
+        const groupOneImgThree = groupOne.querySelector('.-blue-blob');
 
         const tl = new TimelineMax();
 
         tl.set(groupOneImgOne, {opacity: 0})
         tl.set(groupOneImgTwo, {opacity: 0})
         tl.set(groupOneImgThree, {opacity: 0})
-        tl.set(groupTwoImgOne, {opacity: 0})
-        tl.set(groupTwoImgTwo, {opacity: 0})
+        // tl.set(groupTwoImgOne, {opacity: 0})
+        // tl.set(groupTwoImgTwo, {opacity: 0})
         tl.set(groupThreeImgOne, {opacity: 0})
         tl.set(groupThreeImgTwo, {opacity: 0})
         tl.set(groupThreeImgThree, {opacity: 0})
@@ -59,51 +59,52 @@ const HeroAnimation = () => {
             opacity:    0,
             ease:       Power1.easeOut
         }, 7)
-        tl.to(groupTwoImgOne, 1.5, {
-            opacity:    1,
-            ease:       Power1.easeIn
-        }, 9)
-        tl.to(groupTwoImgTwo, 1.5, {
-            opacity:    1,
-            ease:       Power1.easeIn
-        }, 11)
-        tl.to(groupTwoImgOne, 1.5, {
-            opacity:    0,
-            ease:       Power1.easeOut
-        }, 13)
-        tl.to(groupTwoImgTwo, 1.5, {
-            opacity:    0,
-            ease:       Power1.easeOut
-        }, 13)
+        // tl.to(groupTwoImgOne, 1.5, {
+        //     opacity:    1,
+        //     ease:       Power1.easeIn
+        // }, 9)
+        // tl.to(groupTwoImgTwo, 1.5, {
+        //     opacity:    1,
+        //     ease:       Power1.easeIn
+        // }, 11)
+        // tl.to(groupTwoImgOne, 1.5, {
+        //     opacity:    0,
+        //     ease:       Power1.easeOut
+        // }, 13)
+        // tl.to(groupTwoImgTwo, 1.5, {
+        //     opacity:    0,
+        //     ease:       Power1.easeOut
+        // }, 13)
         tl.to(groupThreeImgOne, 1.5, {
             opacity:    1,
             ease:       Power1.easeIn
-        }, 15)
+        }, 9)
         tl.to(groupThreeImgTwo, 1.5, {
             opacity:    1,
             ease:       Power1.easeIn
-        }, 17)
+        }, 11)
         tl.to(groupThreeImgThree, 1.5, {
             opacity:    1,
             ease:       Power1.easeIn
-        }, 19)
+        }, 13)
     }, []);
 
     return (
         <div>
-            <div className = "o-hero__background-group group-one">
-                <img className = "o-hero__background -pink-blob" src = { pinkBlob } />
-                <img className = "o-hero__background -blue-blob-two" src = { blueBlob } />
-                <img className = "o-hero__background -doodles" src = { doodles } />
-            </div>
-            <div className = "o-hero__background-group group-two">
+
+            {/* <div className = "o-hero__background-group group-two">
                 <img className = "o-hero__background -green-paint" src = { greenPaint } />
                 <img className = "o-hero__background -matrix" src = { matrix } />
-            </div>
-            <div className = "o-hero__background-group group-three">
+            </div> */}
+            <div className = "o-hero__background-group group-one">
                 <img className = "o-hero__background -orangsicle" src = { orangSicle } />
                 <img className = "o-hero__background -pink-elephant" src = { pinkElephant } />
                 <img className = "o-hero__background -blue-blob" src = { blueBlob } />
+            </div>
+            <div className = "o-hero__background-group group-three">
+                <img className = "o-hero__background -pink-blob" src = { pinkBlob } />
+                <img className = "o-hero__background -blue-blob-two" src = { blueBlob } />
+                <img className = "o-hero__background -doodles" src = { doodles } />
             </div>
         </div>
     )
