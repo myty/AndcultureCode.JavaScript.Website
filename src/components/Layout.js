6fdb2c7d7b309efd7e2c9be2238e0a7b79ccc675
Zach <zmccleaf@andculture.com>
2020-03-23 21:41:25 -0400

    ogimage update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -52,7 +52,7 @@ const TemplateWrapper = ({ children, pageTitle, data, hideNavigation, scrollTop 
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={logoImg}
+          content="img/apple-touch-icon.png"
         />
       </Helmet>
       <Header
