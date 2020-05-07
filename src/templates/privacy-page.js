a7d27d5f3385f53f24ab405aa7384b82797ab279
Shane Shearer <shane.a.shearer@gmail.com>
2020-05-07 10:23:56 -0400

    AWE-150: A few updates to remove the styling and to accommodate for mobile

--- src/templates/privacy-page.js
+++ src/templates/privacy-page.js
@@ -1,24 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { graphql }                    from 'gatsby';
 import Layout                         from 'components/Layout';
-import ContactHeroGraphic             from 'static/img/contact/contact_hero_graphic.png';
-import HeroBluePaint                  from 'static/img/contact/blue_paint.png';
-import InteriorHero                   from 'components/molecules/InteriorHero';
 import '../assets/scss/app.scss';
 
-export const PrivacyPageTemplate = ({
-  title,
-  secondaryTitle,
-  subTitle,
-  modifier
-}) => (
-  <InteriorHero
-    backgroundImages = { [{ image: ContactHeroGraphic, alt: "Creative swirls and artifacts" }, { image: HeroBluePaint , alt: "Hand stroked blue paint"}] }
-    title            = { title }
-    secondaryTitle   = { secondaryTitle }
-    subTitle         = { subTitle }
-    modifier         = { modifier } />
-)
 
 const PrivacyPage = ({ data }) => {
   const { frontmatter, html } = data.markdownRemark;
@@ -41,15 +25,9 @@ const PrivacyPage = ({ data }) => {
       scrollTop             = { scrollTop }
       showFooterDividerLine = { true }>
       <div className = "p-interior-page">
-        <PrivacyPageTemplate
-          title          = { frontmatter.title }
-          secondaryTitle = { frontmatter.secondaryTitle }
-          subTitle       = { frontmatter.subTitle }
-          modifier       = '-privacy'
-        />
         <main id="main-content" aria-label="Main content">
           <article>
-              <div className="o-rhythm__container">
+              <div className="p-interior-page__wrapper o-rhythm__container">
                 <div className="o-rhythm__row">
                   <div className="p-interior-page__content"
                     dangerouslySetInnerHTML={{
