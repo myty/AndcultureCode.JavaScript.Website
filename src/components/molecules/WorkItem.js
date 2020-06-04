45d139ac672283c9e6eca4ce3181a9924777b384
Shane Shearer <shane.a.shearer@gmail.com>
2020-06-04 09:28:11 -0400

    Adding images and updating the styles for the hover effect on each work item

--- src/components/molecules/WorkItem.js
+++ src/components/molecules/WorkItem.js
@@ -11,25 +11,25 @@ const WorkItem = (props) => {
 
     return (
         <a
-            aria-label = { `Read more about ${post.title}` }
-            className  = {`o-rhythm__row m-work-item ${className}`}
-            href       = { props.url }>
+            aria-label={`Read more about ${post.title}`}
+            className={`o-rhythm__row m-work-item ${className}`}
+            href={props.url}>
             {props.imageOnLeft &&
                 <Fragment>
-                    <WorkItemImage post = { post } />
-                    <WorkItemDetails post = { post } />
+                    <WorkItemImage post={post} />
+                    <WorkItemDetails post={post} />
                 </Fragment>
             }
 
             {!props.imageOnLeft &&
                 <Fragment>
                     <WorkItemDetails
-                        className = "-tablet-and-desktop-only"
-                        post      = { post } />
-                    <WorkItemImage post = { post } />
+                        className="-tablet-and-desktop-only"
+                        post={post} />
+                    <WorkItemImage post={post} />
                     <WorkItemDetails
-                        className = "-phone-only"
-                        post      = { post } />
+                        className="-phone-only"
+                        post={post} />
                 </Fragment>
             }
         </a>
@@ -42,24 +42,24 @@ const WorkItem = (props) => {
 
 const WorkItemDetails = (props) => (
     <div className={`m-work-item__details ${props.className ? props.className : ""}`}>
-        <h2>{ props.post.partnerName }</h2>
-        <h3><span>{ props.post.title }</span></h3>
+        <h2>{props.post.partnerName}</h2>
+        <h3><span>{props.post.title}</span></h3>
     </div>
 )
 
 const WorkItemImage = (props) => (
     <div className="m-work-item__image">
         <img
-            src       = { props.post.listImageArtwork.publicURL }
-            alt       = {`${props.post.title}`}
-            className = "client"
-            data-sal  = "slide-up" />
+            src={props.post.listImageArtwork.publicURL}
+            alt={`${props.post.title}`}
+            className="client"
+            data-sal="fade" />
         <img
-            src         = { props.post.listImageFrame.publicURL }
-            alt         = ""
-            aria-hidden = "true"
-            className   = "frame"
-            data-sal    = "fade" />
+            src={props.post.listImageFrame.publicURL}
+            alt=""
+            aria-hidden="true"
+            className="frame"
+            data-sal="fade" />
     </div>
 );
 
