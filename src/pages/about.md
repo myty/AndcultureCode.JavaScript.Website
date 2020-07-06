233adf002fba4790743babcbe7eb143a62efb669
Shane Shearer <shane.a.shearer@gmail.com>
2020-07-06 14:20:20 -0400

    Current progress with the about page

--- src/pages/about.md
+++ src/pages/about.md
@@ -3,7 +3,6 @@ createPage: true
 templateKey: about-page
 secondaryTitle: hi, we're andculture,
 title: a design company making the world better one experience at a time.
-subTitle: When you come to us, you've already had the, "There has to be a better way!" conversation. Internally, we call it a breaking reality; you can call it, "Why doesn't this work anymore?" Now you need the "A ha!" moment. That's what we excel at; helping you put a finger on what's wrong, and then designing a solution that might not even exist yet.
 seo:
   metaTitle: andculture - a design company in harrisburg, PA
   metaDescription: We are a design company in harrisburg PA that fixes broken realities through cutting edge design, strategy, and engineering solutions.
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,34 +1,24 @@
-import React              from 'react';
-import { graphql }        from 'gatsby';
-import Layout             from '../components/Layout';
-import '../assets/scss/app.scss';
-import ContactHeroGraphic from '../../static/img/contact/contact_hero_graphic.png';
-import HeroBluePaint      from '../../static/img/contact/blue_paint.png';
-import InteriorHero       from '../components/molecules/InteriorHero';
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from 'components/Layout';
+import InteriorHero from 'components/molecules/InteriorHero';
 
-export const AboutPageTemplate = ({
-  title,
-  secondaryTitle,
-  subTitle,
-}) => (
-  <InteriorHero
-    backgroundImages = { [{ image: ContactHeroGraphic, alt: "Creative swirls and artifacts" }, { image: HeroBluePaint , alt: "Hand stroked blue paint"}] }
-    title            = { title }
-    secondaryTitle   = { secondaryTitle }
-    subTitle         = { subTitle } />
-)
+import '../assets/scss/app.scss';
 
 const AboutPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const pageData = data.markdownRemark.frontmatter;
 
   return (
-    <Layout pageTitle = "about" hideNavigation = { true }>
-      <main className = "p-interior-page">
-        <AboutPageTemplate
-          title          = { frontmatter.title }
-          secondaryTitle = { frontmatter.secondaryTitle }
-          subTitle       = { frontmatter.subTitle }
-        />
+    <Layout
+      pageTitle="About"
+      data={pageData}
+      pageClassName="p-careers"
+      showFooterDividerLine={false}>
+      <main className="p-interior-page">
+        <InteriorHero
+          title={pageData.title}
+          subTitle={pageData.subTitle}
+          modifier={"-interior"} />
       </main>
     </Layout>
   )
