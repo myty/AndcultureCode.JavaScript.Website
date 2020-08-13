3902587c2c278364a08ab8ee97eb9fa3976f0870
Aaron Weinberger <aweinberger@andculture.com>
2020-08-13 11:50:41 -0400

    AWE-177 responsive styling improvements

--- src/components/molecules/LearnAboutUs.js
+++ src/components/molecules/LearnAboutUs.js
@@ -5,10 +5,10 @@ const LearnAboutUs = (props) => (
     <div className="o-learn-about-us__container">
         <div className="o-rhythm__container">
             <div className="o-learn-about-us__content-container">
-                <p>
+                <h2>
                     Confident enough to not capitalize our name. Passionate about solving broken realities by looking at
                     all angles. It's real business value delivered through a human-centered lens.
-                </p>
+                </h2>
                 <a href="/about/">LEARN ABOUT US</a>
             </div>
             <div className="o-learn-about-us__image-container">
