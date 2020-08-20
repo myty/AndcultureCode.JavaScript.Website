9a9b724e09d139ac73a62a169dc0265a2a4e5998
Aaron Weinberger <aweinberger@andculture.com>
2020-08-20 14:07:12 -0400

    AWE-177 animation revisions, show half on load

--- src/components/organisms/HeroAnimation.js
+++ src/components/organisms/HeroAnimation.js
@@ -24,258 +24,79 @@ import yellowSquiggle from "./../../../static/img/home/hero-animation/yellow-squ
 
 const HeroAnimation = () => {
     useEffect(() => {
-        const blackXImg = document.querySelector(".-black-x");
-        const blueLinesImg = document.querySelector(".-blue-lines");
-        const bluePlantImg = document.querySelector(".-blue-plant");
-        const fogDotsImg = document.querySelector(".-fog-dots");
-        const fogEyeballImg = document.querySelector(".-fog-eyeball");
-        const fogTagImg = document.querySelector(".-fog-tag");
-        const greenGoImg = document.querySelector(".-green-go");
-        const greenPlantImg = document.querySelector(".-green-plant");
-        const orangeBlobImg = document.querySelector(".-orange-blob");
-        const orangeCubeImg = document.querySelector(".-orange-cube");
-        const pinkAsteriskImg = document.querySelector(".-pink-asterisk");
-        const pinkDropImg = document.querySelector(".-pink-drop");
-        const pinkRaindropsImg = document.querySelector(".-pink-raindrops");
-        const redPrismImg = document.querySelector(".-red-prism");
-        const redTeethImg = document.querySelector(".-red-teeth");
-        const salmonBlobImg = document.querySelector(".-salmon-blob");
-        const tealAsteriskImg = document.querySelector(".-teal-asterisk");
-        const tealRainbowImg = document.querySelector(".-teal-rainbow");
-        const yellowSquiggleImg = document.querySelector(".-yellow-squiggle");
+                        const blackXImg = document.querySelector(".-black-x");
+                        const blueLinesImg = document.querySelector(".-blue-lines");
+                        const bluePlantImg = document.querySelector(".-blue-plant");
+                        const fogDotsImg = document.querySelector(".-fog-dots");
+                        const fogEyeballImg = document.querySelector(".-fog-eyeball");
+                        const fogTagImg = document.querySelector(".-fog-tag");
+                        const greenGoImg = document.querySelector(".-green-go");
+                        const greenPlantImg = document.querySelector(".-green-plant");
+                        const orangeBlobImg = document.querySelector(".-orange-blob");
+                        const orangeCubeImg = document.querySelector(".-orange-cube");
+                        const pinkAsteriskImg = document.querySelector(".-pink-asterisk");
+                        const pinkDropImg = document.querySelector(".-pink-drop");
+                        const pinkRaindropsImg = document.querySelector(".-pink-raindrops");
+                        const redPrismImg = document.querySelector(".-red-prism");
+                        const redTeethImg = document.querySelector(".-red-teeth");
+                        const salmonBlobImg = document.querySelector(".-salmon-blob");
+                        const tealAsteriskImg = document.querySelector(".-teal-asterisk");
+                        const tealRainbowImg = document.querySelector(".-teal-rainbow");
+                        const yellowSquiggleImg = document.querySelector(".-yellow-squiggle");
 
-        
+                        function randomNumber(min, max) {
+                            return Math.random() * (max - min) + min;
+                        }
 
-        var animatedImagesArray = Array.from(document.querySelectorAll(".-hero-animated-image"));
-        console.log(animatedImagesArray);
-        var shuffledAnimatedImages = shuffleArray(animatedImagesArray);
-        console.log(shuffledAnimatedImages);
+                        function shuffleArray(a) {
+                            for (let i = a.length - 1; i > 0; i--) {
+                                const j = Math.floor(Math.random() * (i + 1));
+                                [a[i], a[j]] = [a[j], a[i]];
+                            }
+                            return a;
+                        }
 
-        // for (var i = 0; i < animatedImages.length; i++) {
-        //     TweenMax.from(animatedImages[i], randomNumber(1, 5), {
-        //         opacity: 0,
-        //         yoyo: true,
-        //         delay: randomNumber(0.5, 3.5),
-        //         repeat: -1,
-        //         ease: Linear.easeNone,
-        //     });
-        // }
+                        const tl = new TimelineMax();
 
-        // Choose items to set visible on load
-        // Select random invisible image and transition to visible
-        // select random visible image and transition to visible
+                        var imgArray = [
+                            blackXImg,
+                            blueLinesImg,
+                            bluePlantImg,
+                            fogDotsImg,
+                            fogEyeballImg,
+                            fogTagImg,
+                            greenGoImg,
+                            greenPlantImg,
+                            orangeBlobImg,
+                            orangeCubeImg,
+                            pinkAsteriskImg,
+                            pinkDropImg,
+                            pinkRaindropsImg,
+                            redPrismImg,
+                            redTeethImg,
+                            salmonBlobImg,
+                            tealAsteriskImg,
+                            tealRainbowImg,
+                            yellowSquiggleImg,
+                        ];
 
-        for (var i = 0; i < shuffledAnimatedImages.length / 2; i++) {
-            // var z = document.querySelector(shuffledAnimatedImages[i].toString());
-            // console.log(z);
-        }
+                        imgArray = shuffleArray(imgArray);
+                        for (var i = 0; i < imgArray.length / 2; i++) {
+                            // Set half of the items to be hidden on page load
+                            tl.set(imgArray[i], { opacity: 1 });
+                        }
 
-        function randomNumber(min, max) {
-            return Math.random() * (max - min) + min;
-        }
-        
-        function shuffleArray(a) {
-            for (let i = a.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [a[i], a[j]] = [a[j], a[i]];
-            }
-            return a;
-        }
+                        for (var i = 0; i < imgArray.length; i++) {
+                            TweenMax.from(imgArray[i], randomNumber(2, 7), {
+                                opacity: 0,
+                                yoyo: true,
+                                delay: randomNumber(2, 8),
+                                repeat: -1,
+                                ease: Linear.easeNone,
+                            });
+                        }
 
-        const tl = new TimelineMax();
-
-        // tl.set(blackXImg, { opacity: 0 });
-        // tl.set(blueLinesImg, { opacity: 0 });
-        // tl.set(bluePlantImg, { opacity: 0 });
-        // tl.set(fogDotsImg, { opacity: 0 });
-        // tl.set(fogEyeballImg, { opacity: 0 });
-        // tl.set(fogTagImg, { opacity: 0 });
-        // tl.set(greenGoImg, { opacity: 0 });
-        // tl.set(greenPlantImg, { opacity: 0 });
-        // tl.set(orangeBlobImg, { opacity: 0 });
-        // tl.set(orangeCubeImg, { opacity: 0 });
-        // tl.set(pinkAsteriskImg, { opacity: 0 });
-        // tl.set(pinkDropImg, { opacity: 0 });
-        // tl.set(pinkRaindropsImg, { opacity: 0 });
-        // tl.set(redPrismImg, { opacity: 0 });
-        // tl.set(redTeethImg, { opacity: 0 });
-        // tl.set(salmonBlobImg, { opacity: 0 });
-        // tl.set(tealAsteriskImg, { opacity: 0 });
-        // tl.set(tealRainbowImg, { opacity: 0 });
-        // tl.set(yellowSquiggleImg, { opacity: 0 });
-
-        tl.to(
-            blackXImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            0
-        );
-        tl.to(
-            blueLinesImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            2
-        );
-        tl.to(
-            fogTagImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            4
-        );
-        tl.to(
-            fogDotsImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            6
-        );
-        tl.to(
-            fogEyeballImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            8
-        );
-        tl.to(
-            pinkRaindropsImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            10
-        );
-        tl.to(
-            greenPlantImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            12
-        );
-        tl.to(
-            orangeBlobImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            14
-        );
-        tl.to(
-            greenGoImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            16
-        );
-        tl.to(
-            orangeCubeImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            18
-        );
-        tl.to(
-            pinkAsteriskImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            20
-        );
-        tl.to(
-            pinkDropImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            22
-        );
-        tl.to(
-            bluePlantImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            24
-        );
-        tl.to(
-            redPrismImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            26
-        );
-        tl.to(
-            redTeethImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            28
-        );
-        tl.to(
-            salmonBlobImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            30
-        );
-        tl.to(
-            tealAsteriskImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            32
-        );
-        tl.to(
-            tealRainbowImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            34
-        );
-        tl.to(
-            yellowSquiggleImg,
-            1.5,
-            {
-                opacity: 1,
-                ease: Power1.easeIn,
-            },
-            36
-        );
-    }, []);
+                    }, []);
 
     return (
         <div className="o-hero__image-container">
