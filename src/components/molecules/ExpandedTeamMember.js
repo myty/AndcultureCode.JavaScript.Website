36d7cab96c6c79234dc14fe55532268bf19064e9
Peter Stone <pstone@andculture.com>
2020-09-08 16:20:14 -0400

    desktop expand component is in a good spot, still needs animated and mobile view

--- src/components/molecules/ExpandedTeamMember.js
+++ src/components/molecules/ExpandedTeamMember.js
@@ -10,15 +10,15 @@ const ExpandedTeamMember = (props) => {
   return (
     <div className="expanded-team-member" onClick={props.handleHideExpanded}>
       <div className="expanded-team-member-text">
-          <p>{ props.employee.name }*</p>
-          <p>{ props.employee.position }</p>
-          <p>{ props.employee.easterEgg }</p>
+        <p className="expanded-team-member-text-name">{props.employee.name}*</p>
+        <p className="expanded-team-member-text-position">{props.employee.position}</p>
+        <p className="expanded-team-member-text-egg">* {props.employee.easterEgg}</p>
       </div>
       <div className="expanded-team-member-image">
-            <img
-                src={ props.employee.teamExpandedPhoto.image.childImageSharp.fluid.src }
-                alt={ props.employee.teamExpandedPhoto.description }
-            />
+        <img
+          src={props.employee.teamExpandedPhoto.image.childImageSharp.fluid.src}
+          alt={props.employee.teamExpandedPhoto.description}
+        />
       </div>
     </div>
   );
