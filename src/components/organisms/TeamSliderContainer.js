73b2634951320ce53b41ef7df542eab24001425f
Aaron Weinberger <aweinberger@andculture.com>
2020-10-07 14:32:13 -0400

    Shuffle team grid order
    Co-authored-by: Josh Hughes <jhughes@andculture.com>

--- src/components/organisms/TeamSliderContainer.js
+++ src/components/organisms/TeamSliderContainer.js
@@ -8,60 +8,87 @@ import TeamSlider from "components/organisms/TeamSlider";
 // ------------------------------------
 
 const TeamSliderContainer = (props) => {
+    let initialSlidesPerPage = 3;
 
-  let initialSlidesPerPage = 3;
-
-  function isLargeViewport() {
-    if (typeof window !== `undefined`) {
-      return window.matchMedia("(min-width: 769px)").matches;
+    function isLargeViewport() {
+        if (typeof window !== `undefined`) {
+            return window.matchMedia("(min-width: 769px)").matches;
+        }
+        return false;
     }
-    return false;
-  }
 
+    const [slidesPerPage, setSlidesPerPage] = useState(initialSlidesPerPage);
+    const [isExpanded, setIsExpanded] = useState(false);
+    let isArrowDisabled = isExpanded;
+    const [shuffledItems, setShuffledItems] = useState([]);
 
-  const [slidesPerPage, setSlidesPerPage] = useState(initialSlidesPerPage);
-  const [isExpanded, setIsExpanded] = useState(false);
-  let isArrowDisabled = isExpanded;
+    useEffect(() => {
+        if (shuffledItems.length === 0) {
+            setShuffledItems(shuffleEmployees(props.data));
+        }
 
-  useEffect(() => {
-    function handleResize() {
-      if (isLargeViewport()) {
-        setSlidesPerPage(7);
-        return;
-      }
-      setSlidesPerPage(3);
+        function handleResize() {
+            if (isLargeViewport()) {
+                setSlidesPerPage(7);
+                return;
+            }
+            setSlidesPerPage(3);
+        }
+        handleResize();
+        if (typeof window !== `undefined`) {
+            window.addEventListener("resize", handleResize);
+            return () => {
+                window.removeEventListener("resize", handleResize);
+            };
+        }
+    }, []);
+
+    function shuffleEmployees(a) {
+        let employees = a.allMarkdownRemark.edges;
+        for (let i = employees.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [employees[i], employees[j]] = [employees[j], employees[i]];
+        }
+        a.allMarkdownRemark.edges = employees;
+        return a;
     }
-    handleResize();
-    if (typeof window !== `undefined`) {
-      window.addEventListener("resize", handleResize);
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
+
+    let settings = {
+        dots: true,
+        draggable: true,
+        infinite: true,
+        speed: 500,
+        rows: 4,
+        slidesToShow: slidesPerPage,
+        slidesToScroll: slidesPerPage,
+        prevArrow: (
+            <PrevArrow
+                isDisabled={isArrowDisabled}
+                title="Slide team grid left"
+            />
+        ),
+        nextArrow: (
+            <NextArrow
+                isDisabled={isArrowDisabled}
+                title="Slide team grid right"
+            />
+        ),
+    };
+
+    if (shuffledItems == null || shuffledItems.length === 0) {
+        return null;
     }
-  }, []);
 
-  let settings = {
-    dots: true,
-    draggable: true,
-    infinite: true,
-    speed: 500,
-    rows: 4,
-    slidesToShow: slidesPerPage,
-    slidesToScroll: slidesPerPage,
-    prevArrow: <PrevArrow isDisabled={isArrowDisabled} title="Slide team grid left" />,
-    nextArrow: <NextArrow isDisabled={isArrowDisabled} title="Slide team grid right" />,
-  };
-
-  return (
-    <TeamSlider
-      settings={settings}
-      data={props.data}
-      count={props.count}
-      onExpand={() => setIsExpanded(true)}
-      onCollapse={() => setIsExpanded(false)}
-      isExpanded={isExpanded}
-    />
-  );
+    return (
+        <TeamSlider
+            settings={settings}
+            data={shuffledItems}
+            count={props.count}
+            onExpand={() => setIsExpanded(true)}
+            onCollapse={() => setIsExpanded(false)}
+            isExpanded={isExpanded}
+        />
+    );
 };
 
 // Exports
